5e8e30a6c17f0140cec38fc8b7a07afbaea799c2
ironmonkus <ironmonkus@gmail.com>
2021-11-10 20:39:51 -0500

    Final transition from light to dark

--- index.js
+++ index.js
@@ -4,22 +4,36 @@ let headerEl = document.getElementById("header"); // Header
 let toggleEl = document.getElementById("toggle"); // Toggle
 let bodyEl = document.querySelector("body"); // Body Background
 let h1El = document.getElementById("h1"); // H1
-let cardEl = document.getElementById("post"); // Card background // Card text color
+let postEl = document.getElementById("post"); // Card background // Card text color
+let post1El = document.getElementById("post1"); // Card background // Card text color
+let post2El = document.getElementById("post2"); // Card background // Card text color
 let buttonEl = document.getElementById("btn"); // Button background // Button text color
+let button1El = document.getElementById("btn1"); // Button background // Button text color
+let button2El = document.getElementById("btn2"); // Button background // Button text color
+let labelEl = document.getElementById("label");
 
 function toggle() {
   if (toggleEl.checked) {
-    headerEl.setAttribute("class", "dark");
-    bodyEl.setAttribute("class", "darkbody");
-    // bodyEl.setAttribute("class", "darkbody");
-    // bodyEl.setAttribute("class", "darkbody");
-    // bodyEl.setAttribute("class", "darkbody");
-    // bodyEl.setAttribute("class", "darkbody");
-
-    console.log(bodyEl);
+    headerEl.setAttribute("class", "dark-header");
+    bodyEl.setAttribute("class", "dark-body");
+    h1El.setAttribute("class", "dark-h1");
+    postEl.setAttribute("id", "dark-post");
+    post1El.setAttribute("id", "dark-post");
+    post2El.setAttribute("id", "dark-post");
+    buttonEl.setAttribute("class", "dark-button");
+    button1El.setAttribute("class", "dark-button");
+    button2El.setAttribute("class", "dark-button");
+    labelEl.setAttribute("id", "dark-label");
   } else {
-    bodyEl.removeAttribute("class", "darkbody");
-    headerEl.removeAttribute("class", "dark");
-    console.log(bodyEl);
+    headerEl.setAttribute("class", "header");
+    bodyEl.removeAttribute("class", "dark-body");
+    h1El.removeAttribute("class", "dark-h1");
+    postEl.setAttribute("id", "post");
+    post1El.setAttribute("id", "post");
+    post2El.setAttribute("id", "post");
+    buttonEl.removeAttribute("class", "dark-button");
+    button1El.removeAttribute("class", "dark-button");
+    button2El.removeAttribute("class", "dark-button");
+    labelEl.setAttribute("id", "label");
   }
 }
